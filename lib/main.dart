89c289713bded5efc1ed3a3a12f00a83c0658cb5
Ayobami <oludemiayobami@gmail.com>
2022-12-08 18:21:47 +0100

    Reformat date, add dollar sign and add text field

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 import './transction.dart';
 
 void main() {
@@ -13,84 +15,96 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-
   final List<Transaction> transactions = [
-    Transaction(id: 'a1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(id: 'a1', title: 'Supplies', amount: 39.99, date: DateTime.now())
+    Transaction(
+        id: 'a1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 'a1', title: 'Supplies', amount: 39.99, date: DateTime.now())
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
+      appBar: AppBar(
         title: Text('Flutter App'),
-       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            width: double.infinity,
-            child: Card(
-              child: Text('Chart')
-            ),
-          ),
-          Column(
+      ),
+      body: Column(children: <Widget>[
+        SizedBox(
+          width: double.infinity,
+          child: Card(child: Text('Chart')),
+        ),
+        Column(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: transactions.map((tx) {
               return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
+                  child: Row(children: <Widget>[
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                          )
-                        ),
-                        padding: EdgeInsets.all(10),
-                      child: Text(tx.amount.toString(), style: TextStyle(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${tx.amount.toString()}',
+                      style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20,
                         color: Colors.purple,
-                        ),)),
-                    Column(children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)
-                        ),
-                        child: Text(
-                          tx.title, 
-                          style: TextStyle(
-                            color: Colors.purple,
-                          ),
-                        )
                       ),
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)
-                        ),
+                    )),
+                Card(
+                    child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      TextField(decoration: InputDecoration(labelText: "Title")),
+                      TextField(decoration: InputDecoration(labelText: "Amount")),
+                      TextButton(
+                        onPressed: () {},
                         child: Text(
-                          tx.date.toString(),
-                          style: TextStyle(
-                            color: Colors.purple),
-                        )
-                      )
-                    ],)
-                  ])
-              );}
-            ).toList()
-          ),
-        ]),
+                          "Add Transaction",
+                          style: TextStyle(color: Colors.purple),  
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                        // decoration: BoxDecoration(
+                        //   border: Border.all(color: Colors.purple, width: 2)
+                        // ),
+                        child: Text(
+                      tx.title,
+                      style: TextStyle(
+                          color: Colors.purple,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    )),
+                    Container(
+                        // decoration: BoxDecoration(
+                        //   border: Border.all(color: Colors.purple, width: 2)
+                        // ),
+                        child: Text(
+                      DateFormat('yyyy-MM-dd').format(tx.date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ))
+                  ],
+                )
+              ]));
+            }).toList()),
+      ]),
     );
   }
 }
