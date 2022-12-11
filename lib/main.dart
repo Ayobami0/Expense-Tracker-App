6bca157fa63519f333a85ad57b6d16a0f94dece7
Ayobami <oludemiayobami@gmail.com>
2022-12-11 16:18:12 +0100

    Create models and widjet folders. Add custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-import 'package:intl/intl.dart';
-
-import './transction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/user_transactions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,19 +19,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 'a1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 'a1', title: 'Supplies', amount: 39.99, date: DateTime.now())
-  ];
-
-  // late String titleInput;
-  // late String amountInput;
-
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,86 +29,7 @@ class MyHomePage extends StatelessWidget {
         SizedBox(
           child: Card(child: Text('Chart')),
         ),
-        Card(
-            child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextField(
-                  // onChanged: (value) {
-                  //     titleInput = value;
-                  //   },
-                  controller: titleController,
-                  decoration: InputDecoration(labelText: "Title")),
-              TextField(
-                  // onChanged: (value) {
-                  //     amountInput = value;
-                  //   },
-                  controller: amountController,
-                  decoration: InputDecoration(labelText: "Amount")),
-              TextButton(
-                onPressed: () {
-                    print(titleController.text);
-                    print(amountController.text);
-                  },
-                child: Text(
-                  "Add Transaction",
-                  style: TextStyle(color: Colors.purple),
-                ),
-              ),
-            ],
-          ),
-        )),
-        Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: transactions.map((tx) {
-              return Card(
-                  child: Row(children: <Widget>[
-                Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    )),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${tx.amount.toString()}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    )),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        // decoration: BoxDecoration(
-                        //   border: Border.all(color: Colors.purple, width: 2)
-                        // ),
-                        child: Text(
-                      tx.title,
-                      style: TextStyle(
-                          color: Colors.purple,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16),
-                    )),
-                    Container(
-                        // decoration: BoxDecoration(
-                        //   border: Border.all(color: Colors.purple, width: 2)
-                        // ),
-                        child: Text(
-                      DateFormat('yyyy-MM-dd').format(tx.date),
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ))
-                  ],
-                )
-              ]));
-            }).toList()),
+        TransactionList(),
       ]),
     );
   }
